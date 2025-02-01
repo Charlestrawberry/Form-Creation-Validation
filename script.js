84b76a0e29352e5dcf70daf788430a3f6e457ec6
Oduks <charlestrawb_odus@yahoo.com>
2025-02-01 08:23:45 +0100

    added some functionality

--- script.js
+++ script.js
@@ -8,6 +8,36 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
 
     // retrieve and trim user inputs
-    const username = 
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    let isValid = true;
+    const messages = [];
+
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("invalid input, use a longer username");
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+      isValid = false;
+      messages.push(`put a valid email`);
+    }
+
+    if (password.length < 8) {
+      isValid = false;
+      messages.push(`password should be more 8 character`);
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    } else {
+      feedbackDiv.innerHTML = messages.join("<br>");
+
+      feedbackDiv.style.color = "#dc3545";
+    }
   });
 });
