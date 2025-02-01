16327bc9e4fc4bdf1eb7ae2312155f203cdcf304
Oduks <charlestrawb_odus@yahoo.com>
2025-02-01 03:57:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+  //   add a submit listener to the form
+  form.addEventListener("submit", (event) => {
+    // prevent form from submitting to server
+    event.preventDefault();
+
+    // retrieve and trim user inputs
+    const username = 
+  });
+});
